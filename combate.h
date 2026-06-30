7b1f8c051837327ef00ef5750e9c9d8b008333ed
MIGUEL MASSOLA <miguel_massola@estudante.sesisenai.org.br>
2026-06-30 14:39:10 -0300

    combate atualizado

--- combate.h
+++ combate.h
@@ -7,4 +7,12 @@ void exibir(string nome, int vida, int ataque) {
     cout << "[ " << nome << " ] Vida: " << vida
          << " | Ataque: " << ataque << endl;
 }
+
+int dano(int ataque, int defesa) {
+    int d = ataque - defesa;
+    return (d > 0) ? d : 0;
+}
+
+bool estaVivo(int vida) {
+    return vida > 0;
 }

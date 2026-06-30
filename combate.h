4be7ef03ec6a61a19a58b055baf312660f80dd49
ANTONIO PAULO CABRAL CONTENTE <antonio_contente@estudante.sesisenai.org.br>
2026-06-30 14:32:03 -0300

    Main adicionado blelbe

---
+++ combate.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+void exibir(string nome, int vida, int ataque) {
+    cout << "[ " << nome << " ] Vida: " << vida
+         << " | Ataque: " << ataque << endl;
+}
+}
---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void saudacao() {
+	cout << "Salve salve rapaziadinha" << endl;
+}
+
+void despedida(){
+	cout<<"Tchau, obrigado por usar o programa."<< endl;
+}
+
+int main(){
+	saudacao();
+	cout<<"Projeto em grupo"<< endl;
+	despedida();
+	return 0;
+
+}
